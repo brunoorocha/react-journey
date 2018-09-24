5a21731f9b387c2dbc003a31d6c7515b2a912b91
Bruno Rocha <mail.brunorocha@gmail.com>
2018-09-23 21:08:53 -0300

    Implemented a very basic flexbox based grid system

---
+++ src/components/grid/Col.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+const Col = (props) => (
+    <div className="col">
+        { props.children }
+    </div>
+)
+
+export default Col
--- src/pages/shop-cart/Cart.js
+++ src/pages/shop-cart/Cart.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import { CartContextProvider } from './CartContext'
 import Title from '../../components/title/Title'
+import Subtitle from '../../components/subtitle/Subtitle'
+import Row from '../../components/grid/Row'
+import Col from '../../components/grid/Col'
 
 const Cart = (props) => (
     <CartContextProvider>
-        <Title>My React Shopping Cart</Title>
+        <Row>
+            <Col>
+                <Title>My React Shopping Cart</Title>
+            </Col>            
+        </Row>
+        <Row>
+            <Col>
+                <Subtitle>Grid Colunm 1</Subtitle>
+            </Col>            
+            <Col>
+                <Subtitle>Grid Colunm 2</Subtitle>
+            </Col>   
+            <Col>
+                <Subtitle>Grid Colunm 3</Subtitle>
+            </Col>            
+        </Row>
     </CartContextProvider>
 )
 
